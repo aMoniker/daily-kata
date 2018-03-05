4eabe3546ec3730cb0665833abb3acce11d95f1b
Jim Greenleaf <james.a.greenleaf@gmail.com>
2018-03-05 18:19:49 -0500

    data munging kata

---
+++ 2018-03-05.js
@@ -0,0 +1,29 @@
+/**
+ * Data Munging - Part One
+ *
+ * http://codekata.com/kata/kata04-data-munging/
+ */
+
+import fs from 'fs';
+
+let weatherFile = './data/weather.dat';
+let weatherData = fs.readFileSync(weatherFile).toString();
+
+let rows = weatherData.split(/\n/);
+let headings = rows[0].split(/\s+/).slice(1);
+rows = rows.slice(2, -2).map((row) => row.split(/\s+/).slice(1));
+
+const dayIndex = headings.indexOf('Dy');
+const maxIndex = headings.indexOf('MxT');
+const minIndex = headings.indexOf('MnT');
+
+let minSpread = rows.reduce((values, current) => {
+    let tempSpread = parseInt(current[maxIndex]) - parseInt(current[minIndex]);
+    if (tempSpread < values.spread) {
+        values.day = current[dayIndex];
+        values.spread = tempSpread;
+    }
+    return values;
+}, { day: 0, spread: Infinity });
+
+console.log(`Smallest temperature spread is ${minSpread.spread} degrees on day ${minSpread.day}`);
