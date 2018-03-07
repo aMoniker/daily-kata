7cc9266c5858ee03ca2ae8ff4df94730dc337678
Jim Greenleaf <james.a.greenleaf@gmail.com>
2018-03-07 16:04:43 -0500

    data munging kata part 2

---
+++ kata/2018-03-07.js
@@ -0,0 +1,27 @@
+/**
+ * Data Munging - Part Two
+ *
+ * http://codekata.com/kata/kata04-data-munging/
+ */
+
+import fs from 'fs';
+
+let footballFile = `${__dirname}/../data/football.dat`;
+let footballData = fs.readFileSync(footballFile).toString();
+
+let rows = footballData.split(/\n/);
+rows = rows.slice(1)
+           .filter((row) => !row.match(/---/) && row !== '')
+           .map((row) => row.split(/\s+/).slice(1))
+           ;
+
+let minSpread = rows.reduce((values, current) => {
+    let goalSpread = Math.abs(parseInt(current[6]) - parseInt(current[8]));
+    if (goalSpread < values.spread) {
+        values.team = current[1];
+        values.spread = goalSpread;
+    }
+    return values;
+}, { day: 0, spread: Infinity });
+
+console.log(`Smallest goal spread is ${minSpread.spread} point(s) for team ${minSpread.team}`);
