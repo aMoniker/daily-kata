701ff2c84e3c4e4fdc71de83f5d590b73fefa145
James Greenleaf <james@greenle.af>
2018-04-08 17:06:59 -0400

    update largest palindrome product, added to codewars

--- kata/js/largest-palindrome.js
+++ kata/js/largest-palindrome.js
@@ -1,52 +1,46 @@
 /**
  * Find the largest palindromic product between any two numbers
- * in a given range of numbers.
+ * in a given range of numbers, or return NaN if no palindromic product exists.
+ *
+ * Available as a Codewars kata at:
+ * https://www.codewars.com/kata/5aca2aceb4f6352a4100004f
  */
-const checkPalindrome = (n) => {
-  let numString = ''+n;
-  let pivot = numString.length / 2;
+let checks = 0;
+function largestPalindromicProduct(lower, upper) {
+  console.log(`finding palindromic products between ${lower} and ${upper}`);
+  let largest = 0;
+  let product = 0;
+  for (let i = upper; i >= lower; i--) {
+    for (let j = i; j >= lower; j--) {
+      checks++;
+      product = i * j;
+      if (!isPalindrome(product)) {
+        product = 0;
+        continue;
+      }
+      console.log(`palindrome found: ${i} * ${j} = ${product}`);
+      if (j > lower) { lower = j; }
+      break;
+    }
+    if (product > largest) { largest = product; }
+  }
+  let ret = (largest || NaN);
+  console.log(`largest palindrome between ${lower} & ${upper}: ${ret}`);
+  return ret;
+}
+
+function isPalindrome(n) {
+  let str = n.toString();
+  let pivot = Math.floor(str.length / 2);
   for (let i = 0; i < pivot; i++) {
-    if (numString[i] !== numString[numString.length - i - 1]) {
+    if (str[i] !== str[str.length - i - 1]) {
       return false;
     }
   }
   return true;
-};
-
-let count = 0;
-let largestPalindrome = 0;
-let largestInnerLoop = 0;
-let upperBound = 999;
-let lowerBound = 99;
-
-console.log(`finding palindromic products between ${lowerBound} and ${upperBound}`);
-for (let i = upperBound; i > lowerBound; i--) {
-  if (i <= largestInnerLoop) { break; }
-
-  if (largestInnerLoop > 0) {
-    largestInnerLoop++;
-  }
-
-  let largestPalindromeInner = 0;
-  let innerBottom = (largestInnerLoop || lowerBound);
-  for (let j = i; j > innerBottom; j--) {
-    count++;
-    let prod = i * j;
-
-    if (checkPalindrome(prod) && prod > largestPalindrome) {
-      console.log(`palindrome found: ${i} * ${j} = ${i * j}`);
-      largestPalindromeInner = prod;
-      if (j > largestInnerLoop) {
-        largestInnerLoop = j;
-      }
-      break;
-    }
-  }
-
-  if (largestPalindrome < largestPalindromeInner) {
-    largestPalindrome = largestPalindromeInner;
-  }
 }
 
-console.log(`greatest 3-digit palindrome: ${largestPalindrome}`);
-console.log(`number of checks: ${count}`);
+console.time('took');
+largestPalindromicProduct(1234, 9876);
+console.log(`number of palindrome checks required: ${checks}`);
+console.timeEnd('took');
