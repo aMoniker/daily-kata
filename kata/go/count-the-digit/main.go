7d6af58643d1a066cfb76bf0e4873c88fd96eebb
James Greenleaf <james@greenle.af>
2019-04-21 23:11:20 -0400

    count the digit

---
+++ kata/go/count-the-digit/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("This should be 213:", NbDig(550, 5))
+}
+
+func NbDig(n int, d int) int {
+	digit := strconv.Itoa(d)
+	digits := ""
+	count := 0
+
+	for i := 0; i <= n; i++ {
+		digits = strconv.Itoa(i * i)
+		for _, d := range digits {
+			if string(d) == digit {
+				count++
+			}
+		}
+	}
+
+	return count
+}
