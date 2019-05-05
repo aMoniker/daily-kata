fd85d537af630991223e0ef1b5271e0c9d0093c2
James Greenleaf <james@greenle.af>
2019-05-05 13:58:13 -0400

    Spinning rings

---
+++ kata/python/spinning-rings.py
@@ -0,0 +1,28 @@
+# https://www.codewars.com/kata/59afff65f1c8274f270020f5
+
+
+def spinning_rings(inner_max, outer_max):
+    inner = 0
+    outer = 0
+
+    spins = 0
+    while True:
+        spins += 1
+        inner -= 1
+        if inner < 0:
+            inner = inner_max
+        outer += 1
+        if outer > outer_max:
+            outer = 0
+        if inner == outer:
+            break
+
+    return spins
+
+
+if __name__ == "__main__":
+    assert spinning_rings(2, 3) == 5
+    assert spinning_rings(3, 2) == 2
+    assert spinning_rings(1, 1) == 1
+    assert spinning_rings(2, 2) == 3
+    assert spinning_rings(3, 3) == 2
