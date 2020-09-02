3788075934501f75937a5d44ddf94ed6a20757bf
James Greenleaf <james@greenle.af>
2020-09-02 13:21:43 -0400

    Remove Project Euler problems - These have been moved to a separate, private repo since it's against   the rules to publish solutions to PE problems above #100

--- euler/016.py
+++
@@ -1,9 +0,0 @@
-def main():
-    s = 0
-    for c in str(2 ** 1000):
-        s += int(c)
-    print(s)
-
-
-if __name__ == "__main__":
-    main()
--- euler/048.py
+++
@@ -1,9 +0,0 @@
-def main():
-    n = 0
-    for i in range(1, 1001):
-        n += pow(i, i)
-    print(str(n)[-10:])
-
-
-if __name__ == "__main__":
-    main()
